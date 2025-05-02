503a16ed3db0374f3e58aed622c3e83d8a62e5a3
JvHaeckel <joaohaeckel@gmail.com>
2025-05-02 15:16:45 -0300

    MOBI

--- Learning/Converter/excelToWord.py
+++ Learning/Converter/excelToWord.py
@@ -3,13 +3,25 @@
 import pandas as pd
 from docx import Document
 
+caminho = (r"C:\Users\joaorocha\Desktop\Py\Learning\Converter\Informações.xlsx")
 
+# Ler e carrega os dados do excel
+tabela = pd.read_excel(caminho)
 
+documento = Document()
 
+titulo = input("Qual título do documento? ")
+documento.add_heading(titulo)
 
+# Itera pelas linhas da planilha
+for index, linha in tabela.iterrows():
+    texto = ""
+    for coluna in tabela.columns:
+        texto += f"{coluna}: {linha[coluna]}  |  "
+    documento.add_paragraph(texto.strip(" | "))
 
-
-
+# Salva o documento Word
+documento.save("planilha_convertida.docx")
 
 
 
