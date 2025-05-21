0246d53bd0bac6e7375011570cc805bdb1e0bb9f
JvHaeckel <joaohaeckel@gmail.com>
2025-05-21 15:09:30 -0300

    MOBI

---
+++ Learning/Notas/for7.py
@@ -0,0 +1,9 @@
+
+# Dicionário simples - Print as chaves e valores de um dicionário simples 
+
+dados = { 'nome': 'João' , 'idade': 24, "cidade": 'Recife'}
+
+for chave,valor in dados.items():
+    print(f" {chave} : {valor}" ) # ou print( chave ,':' , valor) 
+
+
