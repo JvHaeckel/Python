6ca6ac306e51db739438adc9358a770dc931131a
JvHaeckel <joaohaeckel@gmail.com>
2025-05-21 17:57:36 -0300

    MOBI

---
+++ Learning/Notas/for10.py
@@ -0,0 +1,14 @@
+# 1. Imprima os números de 1 a 10, um por linha.
+
+numeros = [1,2,3,4,5,6,7,8,9,10]
+
+for numeros in range(10):
+    print(numeros)
+
+
+numeros = [10, 20, 30, 40]
+soma = 0
+
+for num in numeros:
+    soma+= num
+
--- Learning/Notas/for8.py
+++ Learning/Notas/for8.py
@@ -1,7 +1,12 @@
 
-# Contando palavras
+# Usando for in com if para filtrar números maiores que 20
 
 
+numeros = [10, 15, 20, 25, 30]
+
+for num in numeros:
+    if num > 20:
+        print(f"Número grande: {num}")
 
 
 
